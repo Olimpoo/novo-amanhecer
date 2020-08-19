21195870d7731880625472319e2a82027aec6ff8
jupitense <dgutierrez.js@gmail.com>
2020-08-19 18:39:13 -0400

    add hero

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        white: '#ffffff',
+        black: '#222222',
+        heroblack: 'rgba(0,23,31,.33)',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
